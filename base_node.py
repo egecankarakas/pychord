793c2665569c242da05946a9bbd95510837978e2
Tristan <s2508907@leidenuniv.nl>
2021-12-13 23:37:25 +0100

    have working tcp communication

--- base_node.py
+++ base_node.py
@@ -1,6 +1,32 @@
 from abc import ABC, abstractmethod
-class BaseNode(ABC):
-    @abstractmethod
-    find_successor(self, id):
-        pass
+
+import socket
+
+# Create server socket.
+serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+
+# Bind server socket to loopback network interface.
+serv_sock.bind(('10.141.0.30', 6543))
+
+# Turn server socket into listening mode.
+serv_sock.listen(10)
+
+while True:
+    # Accept new connections in an infinite loop.
+    print(".")
+    client_sock, client_addr = serv_sock.accept()
+    print('New connection from', client_addr)
+
+    chunks = []
+    while True:
+        # Keep reading while the client is writing.
+        data = client_sock.recv(2048)
+        if not data:
+            # Client is done with sending.
+            break
+        chunks.append(data)
+
+    client_sock.sendall(b''.join(chunks))
+    client_sock.close()
+
     
--- node.py
+++ node.py
@@ -1,3 +1,35 @@
-import os
+import socket
+import hashlib
 
-os.listdir()
+# Create client socket.
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to server (replace 127.0.0.1 with the real server IP).
+print("right before socket connect")
+client_sock.connect(('10.141.0.30', 6543))
+
+# Send some data to server.
+client_sock.sendall(b'Hello, world')
+client_sock.shutdown(socket.SHUT_WR)
+
+# Receive some data back.
+chunks = []
+while True:
+    data = client_sock.recv(2048)
+    if not data:
+        break
+    chunks.append(data)
+print('Received', repr(b''.join(chunks)))
+
+# Disconnect from server.
+client_sock.close()
+
+
+Class Node:
+    def __init__():
+        self.hostname = socket.gethostname()
+        self.ip = socket.gethostbyname(self.hostname)
+        self.id = 
+        self.position =
+        
+    def 
