99671b83a8136f72bea8c57c2a156425afdaacd5
egeca <egecan@pengutech.com>
2021-12-14 05:30:03 +0100

    Network...

--- base_node.py
+++ base_node.py
@@ -1,6 +1,54 @@
 from abc import ABC, abstractmethod
 class BaseNode(ABC):
-    @abstractmethod
-    find_successor(self, id):
-        pass
+    # @abstractmethod
+    # def find_successor(self, id):
+    #     pass
     
+    # @abstractmethod
+    # def find_predecessor(self,id):
+    #     pass
+
+    # @abstractmethod
+    # def closest_preceding_finger(self,id):
+    #     pass
+
+    @abstractmethod
+    def join(self):
+        # join to chord network
+        pass
+
+    # @abstractmethod
+    # def init_finger_table(self, arbitrary_node):
+    #     # initialize finger table of this node with an arbitrary node
+    #     pass
+
+    # @abstractmethod
+    # def update_others(self):
+    #     # PERIODICAL
+    #     # update all nodes whose finger tables should refer to this node
+    #     pass
+
+    # @abstractmethod
+    # def update_finger_table(self,s,i):
+    #     # PERIODICAL
+    #     # if s is i'th finger of n, update n's finger table with s
+    #     pass
+
+    # # STABILIZATION METHODS
+    # @abstractmethod
+    # def stabilize(self):
+    #     # PERIODICAL
+    #     # periodically verify n's immediate successor, and tell the successor about n
+    #     pass
+
+    # @abstractmethod
+    # def notify(self,predecessor):
+    #     # PERIODICAL
+    #     # predecessor thinks it is this node's predecessor
+    #     pass
+
+    # @abstractmethod
+    # def fix_fingers():
+    #     # PERIODICAL
+    #     # periodically refresh finger table entries
+    #     pass
--- node.py
+++ node.py
@@ -1,3 +1,61 @@
 import os
+import asyncio
+import settings
+import logging
+import socket
+import time
 
-os.listdir()
+from base_node import BaseNode
+
+
+class HostnameFilter(logging.Filter):
+    hostname = socket.gethostname()
+
+    def filter(self, record):
+        record.hostname = HostnameFilter.hostname
+        return True
+
+handler = logging.StreamHandler()
+handler.addFilter(HostnameFilter())
+handler.setFormatter(logging.Formatter('%(asctime)s %(hostname)s: %(message)s', datefmt='%b %d %H:%M:%S'))
+log=logging.getLogger('Chord')
+log.addHandler(handler)
+
+class Node(BaseNode):
+    def __init__(self, ip, bootstrap_server=None) -> None:
+        super().__init__()
+        self.ip=ip
+        self.task_update_periodically=None
+        self.bootstrap_server=bootstrap_server
+        self.joined=False
+    
+    async def run(self,loop):
+        self._server = await asyncio.start_server(self.handle, self.ip, settings.CHORD_PORT)
+        self.task_update_periodically = loop.create_task(self.update_periodically())
+        await self.task_update_periodically
+
+    async def update_periodically(self):
+        log.info('Started Updating Periodically!')
+        while True:
+            if not self.joined:
+                if self.bootstrap_server:
+                    log.info('Joining Chord Ring!')
+                    self.join()
+                else:
+                    log.info('Waiting for others to join the ring!')
+                    time.sleep(1)
+            log.debug('Stabilizing')
+        # pass
+
+    async def stop(self):
+        if self.task_update_periodically:
+            self.task_update_periodically.close()
+
+    async def handle(self, stream_reader, stream_writer):
+        log.debug(f'Reader:{stream_reader} \n Writer:{stream_writer}')
+        pass
+
+    async def join(self):
+        pass
+
+    
